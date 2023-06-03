2dcdfb4c202a3f6a72c7dde22f13f543a5e848fa
jeanfrancoisgratton <jean-francois@famillegratton.net>
2023-06-03 18:50:45 -0400

    Cleaning up a bit

--- src/config/configTemplateCreate.go
+++ src/config/configTemplateCreate.go
@@ -38,41 +38,7 @@ func createExplanationfile() error {
 	"CertificateDirectory" : "/tmp/", -> directory where to write the cert
 	"CertificateName" : "sample_cert", -> cert filename, no extension to the filename
 	"IsCA": true, -> Are we creating a CA or a "normal" server cert ?
-	"Comments": ["To see which values to put in the Usage field, see https://pkg.go.dev/crypto/x509#KeyUsage", "Strip off 'KeyUsage' from the const name and there you go.", "", "Please note that this field offers no functionality and is strictly here for documentation purposes"] -> Those won't appear in the certificate file
-}`
-
-	expFile, err := os.Create(filepath.Join(os.Getenv("HOME"), ".config", "certificatemanager", "template-README.txt"))
-	if err != nil {
-		return err
-	}
-	defer expFile.Close()
-
-	_, err = expFile.WriteString(expText)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// Note : it is a bit ugly: this function is the same as the one above, without the explanations, and with a different filename
-func createSampleTemplate2() error {
-	expText := `{
-	"Country" : "CA",
-	"Provice" : "Quebec",
-	"Locality" : "Blainville",
-	"Organization" : "myorg.net",
-	"OrganizationalUnit" : "myorg",
-	"CommonName" : "myorg.net root CA",
-	"EmailAddresses" : ["certs@myorg.net"],
-	"Duration" : 10,
-	"KeyUsage" : ["Digital Signature", "Certificate Sign", "CRL Sign"],
-	"DNSNames" : ["myorg.net","myorg.com","lan.myorg.net"],
-	"IPAddresses" : ["10.1.1.11"],
-	"CertificateDirectory" : "/tmp/",
-	"CertificateName" : "dev_cert",
-	"IsCA": true,
-	"Comments": ["To see which values to put in the Usage field, see https://pkg.go.dev/crypto/x509#KeyUsage", "Strip off 'KeyUsage' from the const name and there you go.", "", "Please note that this field offers no functionality and is strictly here for documentation purposes"]
+	"Comments": ["To see which values to put in the KeyUsage field, see https://pkg.go.dev/crypto/x509#KeyUsage", "Strip off 'KeyUsage' from the const name and there you go.", "", "Please note that this field offers no functionality and is strictly here for documentation purposes"] -> Those won't appear in the certificate file
 }`
 
 	expFile, err := os.Create(filepath.Join(os.Getenv("HOME"), ".config", "certificatemanager", "template-README.txt"))
