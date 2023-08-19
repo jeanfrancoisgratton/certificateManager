59620d9551aef75ada145dc8204d044bfaf04e60
jeanfrancoisgratton <jean-francois@famillegratton.net>
2023-08-19 11:40:55 -0400

    Initial branch commit

---
+++ src/environment/envHelpers.go
@@ -0,0 +1,87 @@
+// certificateManager
+// Written by J.F. Gratton <jean-francois@famillegratton.net>
+// Original filename: src/environment/envHelpers.go
+// Original timestamp: 2023/08/19 10:02
+
+package environment
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var EnvConfigFile = "defaultEnvConfig.json"
+
+type EnvironmentStruct struct {
+	CertificateRootDir    string `json:"CertificateRootDir"`
+	RootCAdir             string `json:"RootCAdir"`
+	ServerCertsDir        string `json:"ServerCertsDir"`
+	CertificatesConfigDir string `json:"CertificatesConfigDir"`
+	RemoveDuplicates      bool   `json:"RemoveDuplicates"`
+}
+
+func (e EnvironmentStruct) Json2EnvironmentFile() (EnvironmentStruct, error) {
+	var payload EnvironmentStruct
+	var err error
+
+	if !strings.HasSuffix(EnvConfigFile, ".json") {
+		EnvConfigFile += ".json"
+	}
+	rcFile := filepath.Join(os.Getenv("HOME"), ".config", "certificatemanager", EnvConfigFile)
+	jFile, err := os.ReadFile(rcFile)
+	if err != nil {
+		return EnvironmentStruct{}, err
+	}
+	err = json.Unmarshal(jFile, &payload)
+	if err != nil {
+		return EnvironmentStruct{}, err
+	} else {
+		return payload, nil
+	}
+}
+
+func (e EnvironmentStruct) EnvironmentFile2Json(outputfile string) error {
+	if outputfile == "" {
+		outputfile = EnvConfigFile
+	}
+	jStream, err := json.MarshalIndent(e, "", "  ")
+	if err != nil {
+		return err
+	}
+	rcFile := filepath.Join(os.Getenv("HOME"), ".config", "certificatemanager", outputfile)
+	err = os.WriteFile(rcFile, jStream, 0600)
+
+	return err
+}
+
+func CreateSampleEnv() error {
+	//var err error
+	e := EnvironmentStruct{filepath.Join(os.Getenv("HOME"), ".config", "certificatemanager"), "rootCA", "servers", "conf", true}
+	//e := EnvironmentStruct{filepath.Join(os.Getenv("HOME"),".config","certificatemanager"),"certificates", "rootCA", "servers", "conf", true}
+
+	return e.EnvironmentFile2Json(filepath.Join(os.Getenv("HOME"), ".config", "environmentSample.json"))
+}
+
+func CreateExplanationFile() error {
+	exptext := `{
+ "CertificateRootDir" : "$HOME/.config/certificatemanager/certificates  <-- absolute path, always",
+ "RootCAdir" : "rootCA  <-- relative path to CertificateRootDir",
+ "ServerCertsDir" : "servers  <-- relative path to CertificateRootDir",
+ "CertificatesConfigDir" : "conf"  <-- relative path to CertificateRootDir,
+ "RemoveDuplicates": true  <-- should always be set to true, there is no use-case yet to set it to false
+}`
+	expFile, err := os.Create(filepath.Join(os.Getenv("HOME"), ".config", "certificatemanager", "environmentSample-README.txt"))
+	if err != nil {
+		return err
+	}
+	defer expFile.Close()
+
+	_, err = expFile.WriteString(exptext)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- src/main.go
+++ src/main.go
@@ -2,8 +2,50 @@ package main
 
 import (
 	"certificateManager/cmd"
+	"certificateManager/environment"
+	"fmt"
+	"os"
+	"path/filepath"
 )
 
+//func main() {
+//	// First we ensure that the user has a config directory
+//	if err := os.MkdirAll(filepath.Join(os.Getenv("HOME"), ".config", "certificatemanager"), os.ModePerm); err != nil {
+//		fmt.Println(err)
+//		os.Exit(-1)
+//	}
+//	// Second, we create a sample certificate file if none exists
+//	_, err := os.Stat(filepath.Join(os.Getenv("HOME"), ".config", "certificatemanager", "certificatemanagerConf.json"))
+//	if err != nil {
+//		config.CreateTemplatedCertificate()
+//	}
+//	// Third, we create a sample certificate in that config directory
+//	if err := certs.TemplateConfigCreate(); err != nil {
+//		fmt.Println(err)
+//		os.Exit(-1)
+//	}
+//	cmd.Execute()
+//}
+
 func main() {
+	// First, we need to create a configuration directory. This is a per-user config dir
+	if err := os.MkdirAll(filepath.Join(os.Getenv("HOME"), ".config", "certificatemanager"), os.FileMode(0644)); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Second, we create the sample environment file with an explanation file
+	if err := environment.CreateSampleEnv(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if err := environment.CreateExplanationFile(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Third, we create the sample certificate config, and its explanation file
+
+	// We then launch the command loop
 	cmd.Execute()
 }
