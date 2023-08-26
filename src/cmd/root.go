0b2c3a73a9629b0513aeafab9a7f2310a1d026c0
jeanfrancoisgratton <jean-francois@famillegratton.net>
2023-08-26 10:24:56 -0400

    minor refactoring

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -7,6 +7,7 @@ import (
 	"certificateManager/certs"
 	"certificateManager/environment"
 	"certificateManager/helpers"
+	"fmt"
 	"github.com/spf13/cobra"
 	"os"
 )
@@ -44,6 +45,48 @@ var envCmd = &cobra.Command{
 	Short: "Environment sub-command",
 	Run: func(cmd *cobra.Command, args []string) {
 		helpers.Changelog()
+	},
+}
+
+// Lists all certificates in conf directory as per $HOME/.config/certificatemanager/*.json (defined with -e flag)
+var certlistCmd = &cobra.Command{
+	Use:     "list",
+	Aliases: []string{"ls"},
+	Example: "cm cert list",
+	Short:   "Lists all certificates in defined rootDir",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := certs.ListCertificates(); err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+	},
+}
+
+// Verify a given certificate
+var certVerifyCmd = &cobra.Command{
+	Use: "verify",
+	//Aliases: []string{"ls"},
+	Example: "cm cert verify FILENAME",
+	Short:   "Verifies a certificate, as per the provided filename",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := certs.Verify(args); err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+	},
+}
+
+// The biggie: create a CA or "normal" SSL certificate
+var certCreateCmd = &cobra.Command{
+	Use: "create",
+	//Aliases: []string{"ls"},
+	Example: "cm cert create CONFIGFILE",
+	Short:   "Creates a certificate, specifying the config file to use",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := certs.Create(); err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
 	},
 }
 
