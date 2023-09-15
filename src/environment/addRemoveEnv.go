e6ab38e497d2e1f3b6cd67c95e0c2ec523972d3f
jeanfrancoisgratton <jean-francois@famillegratton.net>
2023-09-15 11:14:15 -0400

    Refactored prompt4EnvironmentValues to align it with the new helpers.Get___ functions

--- src/environment/addRemoveEnv.go
+++ src/environment/addRemoveEnv.go
@@ -34,19 +34,19 @@ func AddEnvFile(envfile string) error {
 func prompt4EnvironmentValues() (EnvironmentStruct, error) {
 	var env EnvironmentStruct
 	fmt.Println("The root dir value should be an absolute path, and all other values relative to it")
-	helpers.GetStringValFromPrompt("Enter the certificate root dir (where the PKI directories will sit): ", &env.CertificateRootDir)
+	env.CertificateRootDir = helpers.GetStringValFromPrompt("Enter the certificate root dir (where the PKI directories will sit): ")
 	if !strings.HasPrefix(env.CertificateRootDir, "/") {
 		return EnvironmentStruct{}, helpers.CustomError{Message: fmt.Sprintf("%s %s\n", env.CertificatesConfigDir, helpers.Red("is not an absolute path"))}
 	}
-	helpers.GetStringValFromPrompt("Enter the rootCA directory name: ", &env.RootCAdir)
+	env.RootCAdir = helpers.GetStringValFromPrompt("Enter the rootCA directory name: ")
 	if strings.HasPrefix(env.RootCAdir, "/") {
 		return EnvironmentStruct{}, helpers.CustomError{Message: fmt.Sprintf("%s %s\n", env.RootCAdir, helpers.Red("must be an absolute path"))}
 	}
-	helpers.GetStringValFromPrompt("Enter the servers certificate directory name: ", &env.ServerCertsDir)
+	env.ServerCertsDir = helpers.GetStringValFromPrompt("Enter the servers certificate directory name: ")
 	if strings.HasPrefix(env.RootCAdir, "/") {
 		return EnvironmentStruct{}, helpers.CustomError{Message: fmt.Sprintf("%s %s\n", env.ServerCertsDir, helpers.Red("must be an absolute path"))}
 	}
-	helpers.GetStringValFromPrompt("Enter the servers certificates config directory name: ", &env.CertificatesConfigDir)
+	env.CertificatesConfigDir = helpers.GetStringValFromPrompt("Enter the servers certificates config directory name: ")
 	if strings.HasPrefix(env.RootCAdir, "/") {
 		return EnvironmentStruct{}, helpers.CustomError{Message: fmt.Sprintf("%s %s\n", env.CertificatesConfigDir, helpers.Red("must be an absolute path"))}
 	}
