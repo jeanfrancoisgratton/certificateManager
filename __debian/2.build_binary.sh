6c6b14ff46bcf2cd96877f62744e9f764730991d
jeanfrancoisgratton <jean-francois@famillegratton.net>
2023-10-01 15:49:01 -0400

    Fixed version in deb packaging

--- __debian/2.build_binary.sh
+++ __debian/2.build_binary.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PKGDIR="certificateManager-0.100-0_amd64"
+PKGDIR="certificateManager-1.000-0_amd64"
 
 mkdir -p ${PKGDIR}/opt/bin ${PKGDIR}/DEBIAN
 mkdir -p ${PKGDIR}/opt/bin ${PKGDIR}/DEBIAN
