18fcc016a9ae45de3867eff686e2b2c2977a6831
J.F.Gratton <jean-francois@famillegratton.net>
2023-10-03 11:19:58 -0400

    Fixed issue with the 'strip' binary failing on arm64 arch

--- src/build.sh
+++ src/build.sh
@@ -3,8 +3,16 @@
 GOROOT=/opt/go
 OUTPUT=/opt/bin
 
+arch=$(uname -m)
+
+
 if [ "$#" -gt 0 ]; then
     OUTPUT=$1
 fi
+
 sudo ${GOROOT}/bin/go build -o ${OUTPUT}/cm .
-sudo strip ${OUTPUT}/cm
+
+# Somehow fails on ARM64
+if [ "$arch" = "x86_64" ]; then
+    sudo strip ${OUTPUT}/cm
+fi
